2092571c79abe2f52c2dcb7cd90d8d722edd0002
martmatics <ejohmartins@gmail.com>
2021-07-05 03:45:25 +0100

    week5task

---
+++ server.js
@@ -0,0 +1,26 @@
+const http = require("http");
+const fs = require("fs");
+const url = require('url');
+
+const hostname = '127.0.0.1'
+const port = 5050
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  const q = url.parse(req.url, true);
+  const page = "." + q.pathname;
+  
+    fs.readFile(page, (err, data) => {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+          } 
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write(data);
+          return res.end();
+    })
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`)
+})
